f3df752e97409f6d8e080ed09fe2d9276b94bec8
EpicGamer4088 <tim.platzer@gmx.at>
2023-12-20 20:54:44 +0100

    updated "common.js" "style.css"

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-// Adjust footer position based on content height
+// Funktion zur Anpassung der Footer-Position basierend auf der Inhaltsgröße
 function adjustFooterPosition() {
     var contentHeight = document.body.clientHeight;
     var windowHeight = window.innerHeight;
@@ -12,6 +12,21 @@ function adjustFooterPosition() {
     }
 }
 
-// Adjust footer position on initial load and window resize
-window.addEventListener('load', adjustFooterPosition);
-window.addEventListener('resize', adjustFooterPosition);
+// Event-Listener für die Anpassung der Footer-Position beim Laden und Ändern der Fenstergröße
+window.addEventListener('DOMContentLoaded', function () {
+    adjustFooterPosition();
+    window.addEventListener('resize', adjustFooterPosition);
+});
+
+// Event-Listener für das Verhindern des Standardverhaltens des Formulars und die eigene Validierung
+document.querySelector('form').addEventListener('submit', function(event) {
+    // Hier füge deine Validierungslogik hinzu
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Bitte füllen Sie alle Pflichtfelder aus.');
+        event.preventDefault(); // Verhindere das Absenden des Formulars
+    }
+});
